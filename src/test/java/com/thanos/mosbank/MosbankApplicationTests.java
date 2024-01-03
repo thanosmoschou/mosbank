79fb4f973a4b65dced22d14ae713a774e928ecda
thanosmoschou <130168149+thanosmoschou@users.noreply.github.com>
2024-01-03 17:27:40 +0200

    First Commit

---
+++ src/test/java/com/thanos/mosbank/MosbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanos.mosbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
