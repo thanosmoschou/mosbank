66b15c66a5e439fc409577a2315bc5b62afdcdac
thanosmoschou <130168149+thanosmoschou@users.noreply.github.com>
2024-01-03 18:16:27 +0200

    create controllers

--- src/main/java/com/thanos/mosbank/MosbankApplication.java
+++ src/main/java/com/thanos/mosbank/MosbankApplication.java
@@ -1,12 +1,21 @@
+/*
+ * Author: Thanos Moschou
+ * Description: This is a banking system using Spring Boot
+ * 
+ * Last Modification Date: 3/1/2024
+ */
+
 package com.thanos.mosbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MosbankApplication {
+public class MosbankApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(MosbankApplication.class, args);
 	}
 
