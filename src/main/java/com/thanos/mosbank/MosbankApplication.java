774c50f6e02aed46fce86b9809dacbba6f18f04f
thanosmoschou <thanosmoschou054@gmail.com>
2024-01-03 17:27:40 +0200

    First Commit

---
+++ src/main/java/com/thanos/mosbank/MosbankApplication.java
@@ -0,0 +1,13 @@
+package com.thanos.mosbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosbankApplication.class, args);
+	}
+
+}
